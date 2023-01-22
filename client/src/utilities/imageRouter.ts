d0dd62bf14002ca9b2fba95437febdcdcce8cfb9
Justin <jayscro287@gmail.com>
2023-01-22 11:43:44 -0500

    renamed images dir to assets and changed relevant imports

--- client/src/utilities/imageRouter.ts
+++ client/src/utilities/imageRouter.ts
@@ -1,15 +1,15 @@
-const blackBishop = require('../images/blackBishop.png');
-const whiteBishop = require('../images/whiteBishop.png');
-const blackKing = require('../images/blackKing.png');
-const whiteKing = require('../images/whiteKing.png');
-const blackKnight = require('../images/blackKnight.png');
-const whiteKnight = require('../images/whiteKnight.png');
-const blackPawn = require('../images/blackPawn.png');
-const whitePawn = require('../images/whitePawn.png');
-const blackQueen = require('../images/blackQueen.png');
-const whiteQueen = require('../images/whiteQueen.png');
-const blackRook = require('../images/blackRook.png');
-const whiteRook = require('../images/whiteRook.png');
+const blackBishop = require('../assets/blackBishop.png');
+const whiteBishop = require('../assets/whiteBishop.png');
+const blackKing = require('../assets/blackKing.png');
+const whiteKing = require('../assets/whiteKing.png');
+const blackKnight = require('../assets/blackKnight.png');
+const whiteKnight = require('../assets/whiteKnight.png');
+const blackPawn = require('../assets/blackPawn.png');
+const whitePawn = require('../assets/whitePawn.png');
+const blackQueen = require('../assets/blackQueen.png');
+const whiteQueen = require('../assets/whiteQueen.png');
+const blackRook = require('../assets/blackRook.png');
+const whiteRook = require('../assets/whiteRook.png');
 
 const imageRouter: { [key: string]: string } = {
     'b': blackBishop,
