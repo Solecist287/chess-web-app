e1cf4857cdc67570065650fa3ed1c0effb487d38
Justin <jayscro287@gmail.com>
2023-01-22 11:40:31 -0500

    fixed image loading issues

--- client/src/utilities/imageRouter.ts
+++ client/src/utilities/imageRouter.ts
@@ -1,15 +1,28 @@
+const blackBishop = require('../images/blackBishop.png');
+const whiteBishop = require('../images/whiteBishop.png');
+const blackKing = require('../images/blackKing.png');
+const whiteKing = require('../images/whiteKing.png');
+const blackKnight = require('../images/blackKnight.png');
+const whiteKnight = require('../images/whiteKnight.png');
+const blackPawn = require('../images/blackPawn.png');
+const whitePawn = require('../images/whitePawn.png');
+const blackQueen = require('../images/blackQueen.png');
+const whiteQueen = require('../images/whiteQueen.png');
+const blackRook = require('../images/blackRook.png');
+const whiteRook = require('../images/whiteRook.png');
+
 const imageRouter: { [key: string]: string } = {
-    b: '../images/blackBishop.png',
-    B: '../images/whiteBishop.png',
-    k: '../images/blackKing.png',
-    K: '../images/whiteKing.png',
-    n: '../images/blackKnight.png',
-    N: '../images/whiteKnight.png',
-    p: '../images/blackPawn.png',
-    P: '../images/whitePawn.png',
-    q: '../images/blackQueen.png',
-    Q: '../images/whiteQueen.png',
-    r: '../images/blackRook.png',
-    R: '../images/whiteRook.png',
+    'b': blackBishop,
+    'B': whiteBishop,
+    'k': blackKing,
+    'K': whiteKing,
+    'n': blackKnight,
+    'N': whiteKnight,
+    'p': blackPawn,
+    'P': whitePawn,
+    'q': blackQueen,
+    'Q': whiteQueen,
+    'r': blackRook,
+    'R': whiteRook,
 }
 export default imageRouter;
